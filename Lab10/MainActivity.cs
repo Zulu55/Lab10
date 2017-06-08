62c6ded70b5bb46de9d1e3b5d7c87ab92dcec6d5
Zulu <jzuluaga55@gmail.com>
2017-06-08 17:39:04 -0500

    Finish

---
+++ Lab10/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab10
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = false, Icon = "@drawable/Icon")]
+    public class MainActivity : Activity
+    {
+        int counter = 0;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var contentHeaderTextView = FindViewById<TextView>(Resource.Id.ContentHeaderTextView);
+            contentHeaderTextView.Text = GetText(Resource.String.ContentHeader);
+
+            var clickMeButton = FindViewById<Button>(Resource.Id.ClickMeButton);
+            var clickCounterTextView = FindViewById<TextView>(Resource.Id.ClickCounterTextView);
+
+            clickMeButton.Click += (s, e) =>
+            {
+                counter++;
+                clickCounterTextView.Text = Resources.GetQuantityString(
+                    Resource.Plurals.NumberOfClicks, counter, counter);
+
+                var player = Android.Media.MediaPlayer.Create(this, Resource.Raw.sound);
+            };
+
+            Android.Content.Res.AssetManager manager = this.Assets;
+            using (var reader = new System.IO.StreamReader(manager.Open("Contenido.txt")))
+            {
+                contentHeaderTextView.Text = $"\n\n{reader.ReadToEnd()}";
+            }
+        }
+    }
+}
+
---
+++ Lab10/ValidateActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using SALLab10;
+
+namespace Lab10
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/Icon")]
+    public class ValidateActivity : Activity
+    {
+        EditText EmailText;
+        EditText PasswordText;
+        Button ValidateButton;
+        TextView MessageText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Validate);
+
+            EmailText = FindViewById<EditText>(Resource.Id.EmailText);
+            PasswordText = FindViewById<EditText>(Resource.Id.PasswordText);
+            ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+            MessageText = FindViewById<TextView>(Resource.Id.MessageText);
+
+            ValidateButton.Click += ValidateButton_Click;
+        }
+
+        private async void ValidateButton_Click(object sender, System.EventArgs e)
+        {
+            var serviceClient = new ServiceClient();
+            var studentEmail = EmailText.Text;
+            var password = PasswordText.Text;
+            var myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            var result = await serviceClient.ValidateAsync(studentEmail, password, myDevice);
+            MessageText.Text = $"{result.Status}\n{result.Fullname}\n{result.Token}";
+        }
+    }
+}
